03632cd352cc78daff10018b2149cb164c58da2c
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-03-26 14:15:37 +0530

    Load .ejs files in web-mode
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- init.el
+++ init.el
@@ -142,6 +142,7 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
 (setq web-mode-markup-indent-offset 4)
 (setq web-mode-css-indent-offset 2)
