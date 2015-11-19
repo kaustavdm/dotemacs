7261ba5266e27fe816b1b4253b23c78a1167c900
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-11-19 15:12:00 +0530

    Add OSX specific settings for changing modifier keys
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- init.el
+++ init.el
@@ -219,6 +219,24 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+;; OSX specific settings. Source: Emacs Prelude (https://github.com/bbatsov/prelude)
+(when (eq system-type 'darwin)
+  (defun prelude-swap-meta-and-super ()
+    "Swap the mapping of Meta and Super. Very useful for people using their Mac with a Windows external keyboard
+ from time to time."
+    (interactive)
+    (if (eq mac-command-modifier 'super)
+        (progn
+          (setq mac-command-modifier 'meta)
+          (setq mac-option-modifier 'super)
+          (message "Command is now bound to META and Option is bound to SUPER."))
+      (progn
+        (setq mac-command-modifier 'super)
+        (setq mac-option-modifier 'meta)
+        (message "Command is now bound to SUPER and Option is bound to META."))))
+
+  (global-set-key (kbd "C-c w") 'prelude-swap-meta-and-super))
+
 ;; --------------------------------------------------
 ;; Custom functions
 ;; --------------------------------------------------
