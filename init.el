da15051b35f58074bda6bf9fb835677e586e1f90
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-11-19 15:11:45 +0530

    Simplify package installation
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- init.el
+++ init.el
@@ -78,11 +78,8 @@
   (mapcar
    (lambda (package)
      ;; (package-installed-p 'evil)
-     (if (package-installed-p package)
-         nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-           (package-install package)
-         package)))
+     (if (package-installed-p package) nil
+       (package-install package)))
    packages))
 
 ;; make sure to have downloaded archive description.
