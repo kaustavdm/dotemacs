4a9030ccc7664e81fae4f22a5b01ad5573525b2c
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-11-24 09:41:33 +0530

    Increase default font size to 12
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- init.el
+++ init.el
@@ -40,7 +40,7 @@
 (delete-selection-mode 1)
 
 ;; Set font
-(set-frame-font "Consolas 11")
+(set-frame-font "Consolas 12")
 
 ;; more useful frame title, that show either a file or a
 ;; buffer name (if the buffer isn't visiting a file)
