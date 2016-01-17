40ca129702f0a15cfc00c3b43664b75e1e45d258
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-01-17 21:26:34 +0530

    Use helm for find-files and bookmarks
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- init.el
+++ init.el
@@ -257,6 +257,8 @@
 ;; Helm
 (require 'helm-config)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 (helm-mode 1)
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
